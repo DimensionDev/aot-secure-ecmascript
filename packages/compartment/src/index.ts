7baa17fd5d0e803a7910ec83ca66f836549af8be
Jack Works <jackworks@protonmail.com>
2022-05-25 15:31:29 +0800

    feat: minimal compartment

--- packages/compartment/src/index.ts
+++ packages/compartment/src/index.ts
@@ -1,1 +1,51 @@
-export {}
+import { makeGlobalThis } from './makeGlobalThis.js'
+
+export class Compartment {
+    constructor(
+        endowments?: Record<PropertyKey, unknown>,
+        _modules?: undefined | null | Record<string, unknown>,
+        _options?: CompartmentOptions,
+    ) {
+        if (typeof _options === 'object') {
+            const { name, transforms } = _options
+            if (transforms) throw new TypeError('Compartment: transforms is not supported')
+
+            this.#name = String(name)
+        }
+
+        this.#globalThis = makeGlobalThis(Object.prototype, endowments)
+    }
+    // Should be rejected by CSP.
+    evaluate(source: string) {
+        return this.#globalThis.eval(source)
+    }
+    #globalThis: typeof globalThis
+    get globalThis(): typeof globalThis {
+        return this.#globalThis
+    }
+    // async import(): Promise<unknown> {
+    //     throw new TypeError('To be implemented')
+    // }
+    // importNow(): unknown {
+    //     throw new TypeError('To be implemented')
+    // }
+    // async load(): Promise<unknown> {
+    //     throw new TypeError('To be implemented')
+    // }
+    // module(): ModuleLink {}
+    #name: string | undefined
+    get name() {
+        return this.#name ?? '<unknown>'
+    }
+}
+export interface CompartmentOptions {
+    name?: string
+    // resolveHook?(moduleSpecifier: string, moduleReferrer: string): string
+    // importHook?(moduleSpecifier: string): Promise<ModuleLink>
+    // moduleMapHook?(moduleSpecifier: string): ModuleLink
+    // Not supported since no runtime evaluator is available
+    transforms?: never
+    // globalLexicals?: object
+}
+
+// export type ModuleLink = any
---
+++ packages/compartment/src/makeGlobalThis.ts
@@ -0,0 +1,92 @@
+export function makeGlobalThis(
+    prototype = Object.prototype,
+    endowments: Record<PropertyKey, unknown> | undefined | null,
+): typeof globalThis {
+    const global = Object.create(null)
+
+    Object.defineProperty(global, 'globalThis', {
+        writable: true,
+        configurable: true,
+        value: global,
+    })
+
+    Object.defineProperties(
+        global,
+        cloneFromCurrentCompartment.reduce((previous, name) => {
+            previous[name] = Object.getOwnPropertyDescriptor(globalThis, name)
+            return previous
+        }, Object.create(null)),
+    )
+
+    if (endowments) Object.assign(global, endowments)
+
+    return Object.setPrototypeOf(global, prototype)
+}
+
+// https://tc39.es/ecma262/multipage/global-object.html#sec-global-object
+const cloneFromCurrentCompartment = [
+    'Infinity',
+    'NaN',
+    'undefined',
+
+    // Note: This library has an assumption that NO eval is available due to CSP.
+    // Therefore we use the original version to make it looks like a "native function".
+
+    // This library runs under SES lockdown environment, so we cannot modify Function.prototype.toString to do that.
+    'eval',
+    'isFinite',
+    'isNaN',
+    'parseFloat',
+    'parseInt',
+    'decodeURI',
+    'decodeURIComponent',
+    'encodeURI',
+    'encodeURIComponent',
+
+    'AggregateError',
+    'Array',
+    'ArrayBuffer',
+    'BigInt',
+    'BigInt64Array',
+    'BigUint64Array',
+    'Boolean',
+    'Date', // TODO: virtualize Date?
+    'DataView',
+    'Error',
+    'EvalError',
+    'FinalizationRegistry',
+    'Float32Array',
+    'Float64Array',
+    // Note: Same as eval.
+    'Function',
+    'Int8Array',
+    'Int16Array',
+    'Int32Array',
+    'Map',
+    'Number',
+    'Object',
+    'Promise',
+    'Proxy',
+    'RangeError',
+    'ReferenceError',
+    'RegExp',
+    'Set',
+    // NO SharedArrayBuffer!
+    'String',
+    'Symbol',
+    'SyntaxError',
+    'TypeError',
+    'Uint8Array',
+    'Uint8ClampedArray',
+    'Uint16Array',
+    'Uint32Array',
+    'URIError',
+    'WeakMap',
+    'WeakRef',
+    'WeakSet',
+
+    // NO Atomics (because we don't have SharedArrayBuffer)
+    'JSON',
+    'Math', // TODO: virtualize Math.random?
+    'Reflect',
+]
